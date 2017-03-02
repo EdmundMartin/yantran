6598f56483d58d64e9b93bc4bbea99e0620da434
Edmund Martin <edmartin101@googlemail.com>
2017-03-02 21:39:28 +0000

    Update yantran.py

--- yantran.py
+++ yantran.py
@@ -1,7 +1,31 @@
 import requests
 import json
 
-class yantranslator(object):
+class Yanerrors(Exception):
+    """Base yantran errors class"""
+    pass
+
+class InvalidKey(Yanerrors):
+    """Your API Key is invalid
+
+    Attributes:
+        message -- Yandex refused to accept the provided API Key
+    """
+
+    def __init__(self, message):
+        self.message = message
+
+class BlockedKey(Yanerrors):
+    """Your API Key has been blocked
+
+        Attributes:
+            message -- Yandex has blocked the provided API Key
+        """
+
+    def __init__(self, message):
+        self.message = message
+
+class Yantranslator(object):
 
     def __init__(self,api_key):
         self.api_key = api_key
@@ -10,6 +34,10 @@ class yantranslator(object):
         url = 'https://translate.yandex.net/api/v1.5/tr.json/getLangs?key='
         user_lang = ui_lang
         r = requests.get('{}{}&'.format(url,self.api_key,user_lang))
+        if r.status_code == 401:
+            raise InvalidKey
+        if r.status_code == 402:
+            raise BlockedKey
         data = json.loads(r.text)
         lang_pairs = data['dirs'][0:]
         return lang_pairs
@@ -19,12 +47,20 @@ class yantranslator(object):
         text = '&text={}'.format(text)
         if hint == '':
             r = requests.get('{}{}{}'.format(url,self.api_key,text))
+            if r.status_code == 401:
+                raise InvalidKey
+            if r.status_code == 402:
+                raise BlockedKey
             data = json.loads(r.text)
             language = data['lang'][0:]
             return language
         else:
             hint = '&hint={}'.format(hint)
             r = requests.get('{}{}{}{}'.format(url,self.api_key,text,hint))
+            if r.status_code == 401:
+                raise InvalidKey
+            if r.status_code == 402:
+                raise BlockedKey
             data = json.loads(r.text)
             language = data['lang'][0:]
             return language
@@ -35,6 +71,10 @@ class yantranslator(object):
         text = '&text={}'.format(sentence)
         formatting = '&format={}'.format(formatting)
         r = requests.get('{}{}{}{}{}'.format(url,self.api_key,language_pairing,text,formatting))
+        if r.status_code == 401:
+            raise InvalidKey
+        if r.status_code == 402:
+            raise BlockedKey
         data = json.loads(r.text)
         result = data['text'][0:]
         result = ''.join(result)
@@ -47,6 +87,10 @@ class yantranslator(object):
         formatting = '&format={}'.format(formatting)
         text = '&text={}'.format(sentence)
         r = requests.get('{}{}{}{}{}{}'.format(url,self.api_key,text,language,formatting,options))
+        if r.status_code == 401:
+            raise InvalidKey
+        if r.status_code == 402:
+            raise BlockedKey
         data = json.loads(r.text)
         result = data['text'][0:]
         result = ''.join(result)
